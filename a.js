78561b9d9f90f7a7a54eb1d2253c70df500e0275
Nishant Agarwal <nisagarwal0987@gmail.com>
2022-02-09 12:05:30 +0530

    did stuff

--- a.js
+++ a.js
@@ -1,25 +1,75 @@
-async function getRepos(n, m) {
+async function getInput() {
+    org = document.getElementById("organization").value;
+    numberOfRepos = document.getElementById("number1").value;
+    numberOfForkers = document.getElementById("number2").value;
 
-    clear()
+    // console.log(numberOfRepos + ", " + numberOfForkers);
+    getRepos(numberOfRepos, numberOfForkers, org);
+}
 
-    const user_url = "https://api.github.com/users/google/repos?q=sort=forks&order=desc"
+async function getRepos(n, m, organization) {
+
+    clear();
+
+    for (let x = 1; x <= Math.floor(n/100); x++){
+        const user_url = `https://api.github.com/search/repositories?q=user:\"${organization}\"&page=${x}&sort=forks&order=desc&per_page=100`
+        response = await fetch(user_url)
+        // console.log("fetched")
+        result = await response.json()
+
+        console.log(result)
+
+        const repoResult = document.getElementById("repoResult")
+        
+        for (let i = 0; i < 100; i++){    
+
+            const repoElement = document.createElement("div");
+            repoResult.appendChild(repoElement);
+
+            const anchor = document.createElement("a");
+            anchor.href = result.items[i].html_url;
+            anchor.textContent = result.items[i].full_name;
+            repoElement.appendChild(anchor);
+
+            var button = document.createElement("button");
+            button.innerHTML = "Fetch Forks";
+            repoElement.appendChild(button);
+            button.addEventListener ("click", function() {
+                getOldestForks(result.items[i], m, repoElement);
+            });
+
+            repoElement.appendChild(document.createElement("br"));
+            
+        }
+    }
+    const user_url = `https://api.github.com/search/repositories?q=user:\"${organization}\"&page=${Math.floor(n/100)+1}&sort=forks&order=desc&per_page=100`
     response = await fetch(user_url)
-    console.log("fetched")
     result = await response.json()
 
     console.log(result)
 
     const repoResult = document.getElementById("repoResult")
     
-    for (let i = 0; i < n; i++){    
+    for (let i = 0; i < n%100; i++){    
+
+        const repoElement = document.createElement("div");
+        repoResult.appendChild(repoElement);
 
         const anchor = document.createElement("a");
-        anchor.href = result[i].html_url;
-        anchor.textContent = result[i].full_name;
-        repoResult.appendChild(anchor);
-        repoResult.appendChild(document.createElement("br"));
-    }
+        anchor.href = result.items[i].html_url;
+        anchor.textContent = result.items[i].full_name;
+        repoElement.appendChild(anchor);
 
+        var button = document.createElement("button");
+        button.innerHTML = "Fetch Forks";
+        repoElement.appendChild(button);
+        button.addEventListener ("click", function() {
+            getOldestForks(result.items[i], m, repoElement);
+        });
+
+        repoElement.appendChild(document.createElement("br"));
+        // getOldestForks(result.items[i], m, repoElement);
+    }
 }
 
 function clear() {
@@ -28,12 +78,19 @@ function clear() {
     }
 }
 
-async function getOldestForks(repo, m) {
-    const urll = `https://api.github.com/repos/${repo.owner.login}/${repo.name}/forks?q=sort=oldest`
+async function getOldestForks(repo, m, repoElement) {
+    const urll = `https://api.github.com/repos/${repo.owner.login}/${repo.name}/forks?q=repo:${repo.name}&sort=oldest`
+    console.log(urll);
     let responsee = await fetch(urll)
     resultt = await responsee.json()
 
-    console.log(resultt)
+    console.log(resultt);
+    const ol = document.createElement("ol");
+    for (let i = 0; i < m; i++){
+        ol.innerHTML += `<li><a href = ${resultt[i].owner.html_url}>${resultt[i].owner.login}</a></li>`
+    }
+    
+    repoElement.appendChild(ol);
     return resultt
 }
 
